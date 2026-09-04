47ec0360599acce73ffc16776820d6e8eb4a8928
mayce <49924132+m4yc3x@users.noreply.github.com>
2026-09-04 11:41:55 -0500

    update build to only include necessary dependencies

--- build_exe.bat
+++ build_exe.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal
 title Kabutopz Voice Protocol v7.1 - EXE Builder
+cd /d "%~dp0"
+
+set "BUILD_PYTHON=build\.venv\Scripts\python.exe"
 
 echo ==============================================
 echo   KABUTOPZ VOICE PROTOCOL v7.1
@@ -15,18 +18,29 @@ if errorlevel 1 (
     exit /b 1
 )
 
-python -m pip install --upgrade pip
-if errorlevel 1 goto :error
-
-python -m pip install -r requirements.txt
-if errorlevel 1 goto :error
-
 if exist build rmdir /s /q build
 if exist dist rmdir /s /q dist
 if exist KabutopzVoiceProtocol.spec del /q KabutopzVoiceProtocol.spec
 
-python -m PyInstaller --noconfirm --clean --onedir --noupx --contents-directory "_internal" --name KabutopzVoiceProtocol --windowed --version-file "version_info.txt" --icon "assets\kabutopz_app_icon.ico" --add-data "assets\kabutopz_app_icon.png;assets" --add-data "assets\kabutopz_header_full.png;assets" --add-data "assets\kabutopz_love.png;assets" --add-data "assets\kabutopz_watermark.jpg;assets" --add-data "assets\buy_me_a_coffee.png;assets" starcitizen_voice_keybinds.py
+echo Creating a clean build environment...
+python -m venv "build\.venv"
 if errorlevel 1 goto :error
+
+"%BUILD_PYTHON%" -m pip install --disable-pip-version-check --upgrade pip
+if errorlevel 1 goto :error
+
+"%BUILD_PYTHON%" -m pip install --disable-pip-version-check -r requirements.txt
+if errorlevel 1 goto :error
+
+"%BUILD_PYTHON%" -m PyInstaller --noconfirm --clean --onedir --noupx --contents-directory "_internal" --name KabutopzVoiceProtocol --windowed --version-file "version_info.txt" --icon "assets\kabutopz_app_icon.ico" --exclude-module torch --exclude-module whisper --exclude-module faster_whisper --exclude-module ctranslate2 --exclude-module onnxruntime --exclude-module tensorflow --exclude-module pocketsphinx --exclude-module vosk --add-data "assets\kabutopz_app_icon.png;assets" --add-data "assets\kabutopz_header_full.png;assets" --add-data "assets\kabutopz_love.png;assets" --add-data "assets\kabutopz_watermark.jpg;assets" --add-data "assets\buy_me_a_coffee.png;assets" starcitizen_voice_keybinds.py
+if errorlevel 1 goto :error
+
+rem SpeechRecognition ships offline data and FLAC tools for other systems.
+rem This app uses Google recognition and needs only the Windows FLAC tool.
+if exist "dist\KabutopzVoiceProtocol\_internal\speech_recognition\pocketsphinx-data" rmdir /s /q "dist\KabutopzVoiceProtocol\_internal\speech_recognition\pocketsphinx-data"
+if exist "dist\KabutopzVoiceProtocol\_internal\speech_recognition\flac-linux-x86" del /q "dist\KabutopzVoiceProtocol\_internal\speech_recognition\flac-linux-x86"
+if exist "dist\KabutopzVoiceProtocol\_internal\speech_recognition\flac-linux-x86_64" del /q "dist\KabutopzVoiceProtocol\_internal\speech_recognition\flac-linux-x86_64"
+if exist "dist\KabutopzVoiceProtocol\_internal\speech_recognition\flac-mac" del /q "dist\KabutopzVoiceProtocol\_internal\speech_recognition\flac-mac"
 
 certutil -hashfile "dist\KabutopzVoiceProtocol\KabutopzVoiceProtocol.exe" SHA256 > "dist\KabutopzVoiceProtocol\SHA256.txt"
 
